01cc83b2ca52e0f0b25b7d9c41f66070c99b6da3
mingmingzhang123 <1138195675@qq.com>
2022-12-05 09:28:03 +0800

    feat(新功能): 实现tabbar

---
+++ pages/tabbar/my/my.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		w我的
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
